205e223fb4b34f40cbcff7fd71fe039e3267558c
nabylb <>
2020-09-14 12:14:42 -0700

    Add micro animations

---
+++ src/styles/animations.ts
@@ -0,0 +1,10 @@
+import loading from '../assets/animations/loading/data.json';
+
+interface AnimationNames {
+  loading: string;
+}
+const Animations: AnimationNames = {
+  loading,
+};
+
+export default Animations;
